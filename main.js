7da7ab4a347683d8dff8a88dfca252f9ebeebe57
Wesly Rismahadi <darkking7972@gmail.com>
2025-01-04 01:02:25 +0700

    revisi

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
 let slideIndex = 1;
-    showSlides(slideIndex);
+showSlides(slideIndex);
 
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      let dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-    }
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 
-    function autoSlide() {
-      plusSlides(1);
-      setTimeout(autoSlide, 3000);
-    }
+function autoSlide() {
+  plusSlides(1);
+  setTimeout(autoSlide, 3000);
+}
 
-    document.addEventListener("DOMContentLoaded", () => {
-      autoSlide();
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  autoSlide();
+});
 
-    const menuIcon = document.getElementById("menu-icon");
-    const menuList = document.getElementById("menu-list");
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
 
-    menuIcon.addEventListener("click", () => {
-      menuList.classList.toggle("hidden");
-    });
+menuIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
