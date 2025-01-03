607266b2afd808e7ccd398ffc0a2ec56b2544b28
Wesly Rismahadi <darkking7972@gmail.com>
2025-01-03 20:32:37 +0700

    menambahkan aside serta setting media query

--- main.js
+++ main.js
@@ -34,3 +34,9 @@ let slideIndex = 1;
       autoSlide();
     });
 
+    const menuIcon = document.getElementById("menu-icon");
+    const menuList = document.getElementById("menu-list");
+
+    menuIcon.addEventListener("click", () => {
+      menuList.classList.toggle("hidden");
+    });
